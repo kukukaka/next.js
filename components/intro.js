30fe6ea556aa775a99c96ff85b6065ac51ea83bb
Kike <kukukaka@users.noreply.github.com>
2020-11-15 21:02:30 +0100

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -7,13 +7,7 @@ export default function Intro() {
         kuku
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
+        Random daily toughts
       </h4>
     </section>
   )
