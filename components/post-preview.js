8bf4ae56f3839b5a23a1a35e76af5a98a4d72f14
Kike <kukukaka@users.noreply.github.com>
2020-11-15 20:34:33 +0100

    Update post-preview.js

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,6 +1,4 @@
-import Avatar from '../components/avatar'
 import DateFormatter from '../components/date-formatter'
-import CoverImage from './cover-image'
 import Link from 'next/link'
 
 export default function PostPreview({
