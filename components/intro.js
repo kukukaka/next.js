e80067c253d14b21fabd071093aeb98c18bafdd3
Kike <kukukaka@users.noreply.github.com>
2020-11-17 23:40:18 +0100

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -3,12 +3,9 @@ import { CMS_NAME } from '../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-3xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
-        kuku
+      <h1 className="text-2xl md:text-2xl font-bold tracking-tighter leading-tight md:pr-8">
+        kuku.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8 cuerpo">
-        Mendian larrartean
-      </h4>
     </section>
   )
 }
