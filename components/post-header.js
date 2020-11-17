fb319fbd3e4feeadc2b20a55f5935d01a6e88005
Kike <kukukaka@users.noreply.github.com>
2020-11-17 23:18:11 +0100

    fix

--- components/post-header.js
+++ components/post-header.js
@@ -5,7 +5,7 @@ export default function PostHeader({ title, date }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="max-w-2xl mx-autocabeza">
+      <div className="max-w-2xl mx-auto cabeza">
         <div className="mb-6 text-lg cuerpo">
           <em><DateFormatter dateString={date} /></em>
         </div>
