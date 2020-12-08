efb73cf03217aee241cbd06661b8813d99589ef7
Kike <kukukaka@users.noreply.github.com>
2020-12-08 21:08:00 +0100

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -38,7 +38,7 @@ export default function Meta() {
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@700&family=Source+Sans+Pro:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@700&family=Source+Sans+Pro:ital,wght@0,400;1,400&display=swap" rel="stylesheet" />
     
     </Head>
   )
